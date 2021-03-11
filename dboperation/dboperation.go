ce36f9d1c1efece5ae76d6371e1e62391b92b0a2
TzeHimSung <zxc98567@163.com>
2021-03-11 20:43:43 +0800

    feat(deoperation.go): add database operation

---
+++ dboperation/dboperation.go
@@ -0,0 +1,76 @@
+package dboperation
+
+import (
+	"bufio"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"io"
+	"os"
+	"time"
+)
+
+const (
+	DBConfigPath = "./dboperation/config.json"
+)
+
+type User struct {
+	Id   sql.NullInt64  `db:"id"`
+	Name sql.NullString `db:"name"`
+}
+
+func readDBConfig() (result string) {
+	file, err := os.Open(DBConfigPath)
+	defer file.Close()
+	if err != nil {
+		panic(err)
+	}
+	buf := bufio.NewReader(file)
+	for {
+		s, err := buf.ReadString('\n')
+		result += s
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("Read is ok")
+				break
+			} else {
+				fmt.Println("ERROR:", err)
+				return
+			}
+		}
+	}
+	return result
+}
+
+func Dbtest() {
+	var dbConfigStr struct {
+		Username string
+		Password string
+		Network  string
+		Server   string
+		Port     int
+		Database string
+	}
+	dbConfig := readDBConfig()
+	err := json.Unmarshal([]byte(dbConfig), &dbConfigStr)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(dbConfigStr)
+	user := new(User)
+	dsn := fmt.Sprintf("%s:%s@%s(%s:%d)/%s", dbConfigStr.Username, dbConfigStr.Password, dbConfigStr.Network,
+		dbConfigStr.Server, dbConfigStr.Port, dbConfigStr.Database)
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		panic(err)
+	}
+	db.SetConnMaxLifetime(100 * time.Second)
+	db.SetMaxOpenConns(100)
+	db.SetMaxIdleConns(16)
+	res := db.QueryRow("SELECT * FROM table1")
+	if err := res.Scan(&user.Id, &user.Name); err != nil {
+		panic(err)
+	}
+	fmt.Println(*user)
+}
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"prometheus/dboperation"
 	"prometheus/router"
 
 	"github.com/kataras/iris/v12"
@@ -10,6 +11,8 @@ func main() {
 	app := iris.New()
 
 	router.RouteInit(app)
+
+	dboperation.Dbtest()
 
 	if err := app.Run(iris.Addr(":8000")); err != nil {
 		panic(err)
