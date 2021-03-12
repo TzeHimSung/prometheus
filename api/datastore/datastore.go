fac1fffe04a363029844328f552954c9d6987a16
TzeHimSung <zxc98567@163.com>
2021-03-12 22:11:00 +0800

    fix(datastore.go): remove duplicated file suffix

--- api/datastore/datastore.go
+++ api/datastore/datastore.go
@@ -24,14 +24,13 @@ func GetDataStoreProjectList() []ProjectInfo {
 }
 
 func GetDataStoreInfo() ([]DataStoreInfo, []FileSuffixInfo) {
-	fileList, fileSuffixList := make([]DataStoreInfo, 0, 10), make([]FileSuffixInfo, 0, 10)
+	fileList, fileSuffixList := make([]DataStoreInfo, 0, 10), make([]string, 0, 10)
 
 	dir, err := ioutil.ReadDir("./uploads/data")
 	if err != nil {
 		panic(err)
 	}
 
-	suffixCount := 0
 	for _, file := range dir {
 		if file.IsDir() {
 			continue
@@ -42,20 +41,21 @@ func GetDataStoreInfo() ([]DataStoreInfo, []FileSuffixInfo) {
 			Status:     "已上传",
 			CreateTime: "2021-03-08 00:00:00",
 		})
-		fileSuffixList = append(fileSuffixList, FileSuffixInfo{
-			Id:   suffixCount,
-			Name: strings.Split(file.Name(), ".")[len(strings.Split(file.Name(), "."))-1],
-		})
-		suffixCount++
+		fileSuffixList = append(fileSuffixList, strings.Split(file.Name(), ".")[len(strings.Split(file.Name(), "."))-1])
 	}
 
 	resultFileSuffixList := make([]FileSuffixInfo, 0, len(fileSuffixList))
-	tempMap := map[FileSuffixInfo]struct{}{}
+	tempMap := map[string]struct{}{}
 
+	suffixCount := 0
 	for _, item := range fileSuffixList {
 		if _, ok := tempMap[item]; !ok {
 			tempMap[item] = struct{}{}
-			resultFileSuffixList = append(resultFileSuffixList, item)
+			resultFileSuffixList = append(resultFileSuffixList, FileSuffixInfo{
+				Id:   suffixCount,
+				Name: item,
+			})
+			suffixCount++
 		}
 	}
 
