59c9f22c693059336f6a04be2410cd660f4f9b97
TzeHimSung <zxc98567@163.com>
2021-03-26 23:30:46 +0800

    fix(model.go): fix xorm varchar tags

--- model/model.go
+++ model/model.go
@@ -13,22 +13,22 @@ type FileSuffixInfo struct {
 }
 
 type DataStoreInfo struct {
-	FileName   string `json:"fileName" xorm:"Varchar(255)"`
-	Source     string `json:"source" xorm:"Varchar(255)"`
-	Status     string `json:"status" xorm:"Varchar(255)"`
+	FileName   string `json:"fileName" xorm:"varchar(255)"`
+	Source     string `json:"source" xorm:"varchar(255)"`
+	Status     string `json:"status" xorm:"varchar(255)"`
 	CreateTime string `json:"createTime" xorm:"created"`
 }
 
 type ModelStoreInfo struct {
-	FileName   string `json:"fileName" xorm:"Varchar(255)"`
-	Source     string `json:"source" xorm:"Varchar(255)"`
-	Status     string `json:"status" xorm:"Varchar(255)"`
+	FileName   string `json:"fileName" xorm:"varchar(255)"`
+	Source     string `json:"source" xorm:"varchar(255)"`
+	Status     string `json:"status" xorm:"varchar(255)"`
 	CreateTime string `json:"createTime" xorm:"created"`
 }
 
 type RunningModel struct {
 	Id         int    `xorm:"Int"`
-	ScriptName string `xorm:"Varchar(255)"`
+	ScriptName string `xorm:"varchar(255)"`
 	Ctx        *context.Context
 	CancelFunc *context.CancelFunc
 }
