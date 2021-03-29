012c2f848cbe28613049842e0aba4a5c472456eb
TzeHimSung <zxc98567@163.com>
2021-03-29 22:50:38 +0800

    feat(modeltraining.go): add goroutine support in LaunchModel

--- api/modeltraining/modeltraining.go
+++ api/modeltraining/modeltraining.go
@@ -5,6 +5,8 @@ import (
 	"github.com/kataras/golog"
 	"os"
 	"os/exec"
+	"prometheus/api/database"
+	. "prometheus/model"
 	"strings"
 	"time"
 )
@@ -15,52 +17,64 @@ const (
 	OutputTimeFormat = "2006-01-02-15-04-05"
 )
 
-func LaunchModel(filename string) {
-	golog.Info("Launch Model file: " + filename)
-
-	// create output path
-	// path name should be replaced
-	outputPath := OutputRootPath + "model-" + strings.Replace(filename, ".", "-", -1) + "-" + time.Now().Format(OutputTimeFormat)
-	_, err := os.Stat(outputPath)
-	if err != nil {
-		err = os.Mkdir(outputPath, 0666)
-	}
-	golog.Info("Output path check passed. Launching model...")
-
-	// launch model
-	modelPath := "./uploads/model/" + filename
-	cmd := exec.Command("python", modelPath)
-	out, err := cmd.Output()
-	if err != nil {
-		golog.Info("Error in model launching process.")
-		panic(err)
-	}
-	golog.Info("Model launched. Create output file...")
-
-	// create output file
-	f, err := os.Create(outputPath + "/output.txt")
-	defer f.Close()
-	if err != nil {
-		panic(err)
-	} else {
-		// write output
-		_, err := f.Write(out)
-		if err != nil {
-			panic(err)
-		}
-		golog.Info("Output file is created.")
-	}
-}
-
-func LaunchTest(ctx context.Context) {
+func LaunchModel(filename string, modelID int, ctx context.Context) {
 	for {
 		select {
 		case <-ctx.Done():
-			golog.Println("Launch process is canceled.")
+			golog.Warn("Model " + filename + "is canceled.")
 			return
 		default:
-			golog.Println("Launch process is running")
-			time.Sleep(1 * time.Second)
+			golog.Info("Launch Model file: " + filename)
+
+			// create output dir
+			outputPath := OutputRootPath + "model-" + strings.Replace(filename, ".", "-", -1) + "-" + time.Now().Format(OutputTimeFormat)
+			_, err := os.Stat(outputPath)
+			if err != nil {
+				err = os.Mkdir(outputPath, 0666)
+			}
+			golog.Info("Output path check passed. Launching model...")
+
+			// launch model
+			modelPath := "./uploads/model/" + filename
+			cmd := exec.Command("python", modelPath)
+			golog.Info("Model is running...")
+			out, err := cmd.Output()
+			if err != nil {
+				golog.Info("Error in model launching process.")
+				panic(err)
+			}
+			golog.Info("Model launched. Create output file...")
+
+			// create output file
+			f, err := os.Create(outputPath + "/output.txt")
+			defer f.Close()
+			if err != nil {
+				panic(err)
+			} else {
+				// write output
+				_, err := f.Write(out)
+				if err != nil {
+					panic(err)
+				}
+				golog.Info("Output file is created.")
+			}
+
+			// remove running model log
+			var modelIdx int = 0
+			for idx, runningModel := range RunningModelList {
+				if runningModel.Id == modelID {
+					// add finished log
+					_, err := database.AddFinishedModelLog(runningModel.Id, runningModel.ScriptName, runningModel.LaunchTime)
+					if err != nil {
+						panic(err)
+					}
+					modelIdx = idx
+					break
+				}
+			}
+			RunningModelList = append(RunningModelList[:modelIdx], RunningModelList[modelIdx+1:]...)
+
+			return
 		}
 	}
 }
