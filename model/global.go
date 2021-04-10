ce9157eb0debb218c4ea30098239e8790ccc7c98
TzeHimSung <zxc98567@163.com>
2021-04-10 23:24:55 +0800

    chore(global.go): rewrite comment

--- model/global.go
+++ model/global.go
@@ -1,26 +1,27 @@
+// Package model
 /**
  * @Description: global const or variable
  */
 package model
 
 const (
-	// standard time format
+	// TimeFormat standard time format
 	TimeFormat = "2006-01-02 15:04:05"
-	// project file path
+	// ProjectPath project file path
 	ProjectPath = "./uploads/project"
-	// data file path
+	// DataPath data file path
 	DataPath = "./uploads/data"
-	// model file path
+	// ModelPath model file path
 	ModelPath = "./uploads/model"
-	// model output path
+	// ModelOutputPath model output path
 	ModelOutputPath = "./runmodel/output"
 )
 
 var (
-	// current project
+	// CurrProject current project
 	CurrProject = "Sample Project"
-	// model id counter
+	// ModelID model id counter
 	ModelID = 0
-	// running model list
+	// RunningModelList running model list
 	RunningModelList = make([]RunningModel, 0)
 )
