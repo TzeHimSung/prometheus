726d0f0f80c5d44b9ac0bc12c796f612d3387f49
TzeHimSung <zxc98567@163.com>
2021-04-11 22:19:16 +0800

    refactor(dbModel.go): remove DataStoreInfo and ModelStoreInfo struct

--- model/dbModel.go
+++ model/dbModel.go
@@ -12,20 +12,6 @@ type Project struct {
 	CreateTime  time.Time `json:"createTime" xorm:"created"`       // project create time
 }
 
-//type DataStoreInfo struct {
-//	FileName   string    `json:"fileName" xorm:"varchar(255)"` // file name
-//	Source     string    `json:"source" xorm:"varchar(255)"`   // upload source
-//	Status     string    `json:"status" xorm:"varchar(255)"`   // file status
-//	CreateTime time.Time `json:"createTime" xorm:"created"`    // file create time (or upload time)
-//}
-//
-//type ModelStoreInfo struct {
-//	FileName   string    `json:"fileName" xorm:"varchar(255)"` // file name
-//	Source     string    `json:"source" xorm:"varchar(255)"`   // upload source
-//	Status     string    `json:"status" xorm:"varchar(255)"`   // file status
-//	CreateTime time.Time `json:"createTime" xorm:"created"`    // file create time (or upload time)
-//}
-
 type FileInfo struct {
 	ProjectName string    `json:"projectName" xorm:"varchar(255)"` // project name
 	FileName    string    `json:"fileName" xorm:"varchar(255)"`    // file name
@@ -36,7 +22,15 @@ type FileInfo struct {
 type FinishedModelInfo struct {
 	Id         int       `json:"id" xorm:"int"`                  // model id
 	ScriptName string    `json:"scriptName" xorm:"varchar(255)"` // model name
-	Status     string    `json:"status" xorm:varchar(255)`       // status
+	Status     string    `json:"status" xorm:"varchar(255)"`     // status
 	LaunchTime time.Time `json:"launchTime" xorm:"created"`      // model launch time
 	FinishTime time.Time `json:"finishTime" xorm:"created"`      // model finish time
+}
+
+type FinishedProjectInfo struct {
+	Id          int       `json:"id" xorm:"int"`                   // project id
+	ProjectName string    `json:"projectName" xorm:"varchar(255)"` // project name
+	Status      string    `json:"status" xorm:"varchar(255)"`      // project status
+	LaunchTime  time.Time `json:"launchTime" xorm:"DateTime"`      // project launch time
+	FinishTime  time.Time `json:"finishTime" xorm:"created"`       // project finish time
 }
