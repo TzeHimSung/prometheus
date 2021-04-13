2411495252bf183eb7f91fca7ddfe486432ee5bd
TzeHimSung <zxc98567@163.com>
2021-04-13 11:05:13 +0800

    chore(template): add template project

---
+++ template/toolbox/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import os
+
+ProjectName = "test3"
+
+ProjectSavePath = os.path.join(".", "uploads", "project")
+ProjectRootPath = os.path.join(ProjectSavePath, ProjectName)
